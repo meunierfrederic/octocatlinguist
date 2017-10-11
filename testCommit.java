cea4e0f6543ad34708bf1c9274d28082be8fcd53
meunierfrederic <32485627+meunierfrederic@users.noreply.github.com>
2017-10-11 16:56:35 +0200

    Update testCommit.java

--- testCommit.java
+++ testCommit.java
@@ -1,4 +1,7 @@
 // a new file was added
+
+// Je commente la void main pour voir 
 public static void main() {
-System.out.println("Hello world!");
+  System.out.println("Hello world!");
+  System.out.println("Avec des commentaires cette fois-ci");
 }
